8df3f1ca1a82232323778c8fa013c32ce8c3df9f
UnicornStudio <40837337+UnicornStudio@users.noreply.github.com>
2021-09-07 17:12:46 -0600

    first commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,25 @@
 import { Link } from "react-scroll";
+import { useState } from "react";
 
 const NavBar = () => {
+    const [showNav, setShowNav] = useState(false);
+    function toggleNav() {
+      if (showNav == true) {
+        setShowNav(false);
+      } else {
+        setShowNav(true);
+      }
+    }
     return (
-        <div className="navbar">
-            <Link className="nav-link" to="about" spy={true} smooth={true}>ABOUT</Link>
-            <Link className="nav-link" to="webdev" spy={true} smooth={true}>WEB DEV</Link>
-            <Link className="nav-link" to="game" spy={true} smooth={true}>GAME DEV</Link>
-            <Link className="nav-link" to="art" spy={true} smooth={true}>ART</Link>
-            <Link className="nav-link" to="photos" spy={true} smooth={true}>PHOTOGRAPHY</Link>
-            <Link className="nav-link" to="contact" spy={true} smooth={true}>CONTACT</Link>         
+        <div className={`navbar ${showNav ? "navbar-left" : "navbar-normal"}`}>
+            <div className={`nav-links-container ${showNav ? "nav-links-show" : "nav-links-hide"}`}>
+                <Link className="nav-link" to="about" spy={true} smooth={true}>ABOUT</Link>
+                <Link className="nav-link" to="webdev" spy={true} smooth={true}>WEB DEV</Link>
+                <Link className="nav-link" to="game" spy={true} smooth={true}>GAME DEV</Link>
+                <Link className="nav-link" to="art" spy={true} smooth={true}>ART</Link>
+                <Link className="nav-link" to="photos" spy={true} smooth={true}>PHOTOGRAPHY</Link>
+            </div>
+            <div onClick={toggleNav}><i className="fas fa-bars nav-button"></i></div>
         </div>
     )
 }
